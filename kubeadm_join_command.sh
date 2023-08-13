c1b03a4b0f03f59e38280c5f0d3a63d44dc2a770
mickyBaisil <120027056+mickyBaisil@users.noreply.github.com>
2023-08-13 16:10:08 -0400

    Delete kubeadm_join_command.sh

--- kubeadm_join_command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo kubeadm join 10.0.1.189:6443 --token kn9t0i.9xxo5pmn7kmspna0 --discovery-token-ca-cert-hash sha256:94f406c5e98edc43b808a446e1c2a34d48034c1b11a0fbf102d87f667b9c26f0 
